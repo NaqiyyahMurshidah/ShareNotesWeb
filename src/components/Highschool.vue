f3a3b3b29780f019df07dfc2a0b71c438da81138
NaqiyyahMurshidah <148673027+NaqiyyahMurshidah@users.noreply.github.com>
2023-12-27 01:14:33 +0800

    Update Highschool.vue

--- src/components/Highschool.vue
+++ src/components/Highschool.vue
@@ -1,58 +1,53 @@
-<script setup></script>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
 
 <template>
-  <div class="min-h-screen min-w-full bg-yellow-100">
-    <div class="flex flex-row flex-wrap justify-center p-[9rem] gap-20">
+  <div class="min-h-screen min-w-full ">
+    <div
+      class="p-[20px] font-serif flex flex-col items-center mt-10"
+    >
+    <div > Notes: </div>
       <RouterLink to="/math">
         <div
-          class="rounded-full cursor-pointer bg-gray-200 border border-black hover:bg-purple-300 active:bg-purple-900 h[200px] w-[190px] h-full"
+          class="w-[800px] hover:bg-zinc-200 bg-zinc-100 border p-[12px] text-[18px]"
         >
-          <p class="px-[30px] py-[75px] font-serif font-bold text-xl">
+          <p class="">
             Mathematic
           </p>
-        </div></RouterLink
-      >
+          <h6 class="text-sm"> description </h6>
+        </div>
+      </RouterLink>
 
       <RouterLink to="/historyy">
         <div
-          class="rounded-full cursor-pointer bg-gray-200 border border-black hover:bg-pink-300 active:bg-pink-900 h-[200px] w-[190px]"
+          class="w-[800px] hover:bg-zinc-200 bg-zinc-100 border p-[12px] text-[18px]"
         >
-          <p class="px-[55px] py-[75px] font-serif font-bold text-xl">
+          <p class="">
             History
           </p>
+          <h6 class="text-sm"> description </h6>
         </div></RouterLink
       >
 
-      <RouterLink to="/chem">
+      <RouterLink to="/chemistry">
         <div
-          class="rounded-full cursor-pointer bg-gray-200 border border-black hover:bg-red-300 active:bg-red-900 h-[200px] w-[190px]"
+          class="w-[800px] hover:bg-zinc-200 bg-zinc-100 border p-[12px] text-[18px]"
         >
-          <p class="px-[30px] py-[75px] font-serif font-bold text-xl">
+          <p class="">
             Chemistry
           </p>
+          <h6 class="text-sm"> description </h6>
         </div></RouterLink
       >
-
       <RouterLink to="/biology">
         <div
-          class="rounded-full cursor-pointer bg-gray-200 border border-black hover:bg-green-300 active:bg-green-900 h-[200px] w-[190px]"
+          class="w-[800px] hover:bg-zinc-200 bg-zinc-100 border p-[12px] text-[18px]"
         >
-          <p
-            class="px-[30px] py-[75px] font-serif font-bold text-xl text-center"
-          >
+          <p class="">
             Biology
           </p>
-        </div></RouterLink
-      >
-      <RouterLink to="/phys">
-        <div
-          class="rounded-full cursor-pointer bg-gray-200 border border-black hover:bg-blue-300 active:bg-blue-900 h-[200px] w-[190px]"
-        >
-          <p
-            class="px-[30px] py-[75px] font-serif font-bold text-xl text-center"
-          >
-            Physics
-          </p>
+          <h6 class="text-sm"> description </h6>
         </div></RouterLink
       >
     </div>
